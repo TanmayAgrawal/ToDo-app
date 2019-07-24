7922afece8cfdd3a9ae61b4e00f3e91b41b09b1d
TanmayAgrawal <f20180728@pilani.bits-pilani.ac.in>
2019-07-24 12:07:27 +0530

    displaying data

--- todo.js
+++ todo.js
@@ -2,3 +2,19 @@ var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost/todo';
+var str = "";
+app.route('/').get(function(req,res) {
+    MongoClient.connect(url,function(err,client) {
+        var db = client.db('todoDB');
+        var cursor = db.collection('todo').find();
+        cursor.forEach(function(item) {
+            if(item != null) {
+                str = str + item + "</br>";
+            }
+        }, function(err) {
+            res.send(str);
+            client.close();
+        });
+    });
+});
+app.listen(3000, function());
